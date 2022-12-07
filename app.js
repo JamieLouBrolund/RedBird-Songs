364868cc13be26964cab01a0eed3afda5d509b09
Jamie Lou Brolund <94914187+JamieLouBrolund@users.noreply.github.com>
2022-12-07 11:32:11 -0600

    updated nodemon script

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 connectDB().then(()=> {
   app.listen(
     PORT,
-    console.log(`Server running on ${process.env.NODE_ENV} mode on PORT ${PORT}`)
+    console.log(`Server running`)
   );
 });
 

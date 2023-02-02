d0a9fc0d0f4c4a529af165cdc93fb9fad81ed069
Jamie Lou Brolund <94914187+JamieLouBrolund@users.noreply.github.com>
2023-02-02 12:41:28 -0600

    removed keys from callback

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: keys.callback + "/auth/google/callback",
+        callbackURL: "/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
